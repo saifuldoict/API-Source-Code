8c02c32da98bdd7a08a3d8c7e25fd9b1a7d9e6a9
Md. Saiful Islam <saiful.doict@gmail.com>
2024-12-28 21:09:42 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const express =require('express');
+const router =require('./src/routes/api');
+const app= new express();
+
+const rateLimit =require('express-rate-limit');
+const helmet =require('helmet');
+const mongoSanitize =require('express-mongo-sanitize');
+
+const xss =require('xss-clean');
+const hpp =require('hpp');
+const cors =require('cors');
+const cookieParser = require('cookie-parser');
+const mongoose =require('mongoose');
+const path = require("path");
+
+
+let URL="mongodb+srv://saifulnhk:5198MMfDlyA4xDoT@cluster0.r1yrc.mongodb.net/MERN"
+mongoose.connect(URL).then((res)=>{
+    console.log("Database Connected")
+}).catch((err)=>{
+    console.log(err)
+})
+
+
+app.use(cookieParser());
+app.use(cors())
+app.use(helmet())
+app.use(mongoSanitize())
+app.use(xss())
+app.use(hpp())
+
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
+
+
+const limiter= rateLimit({windowMs:15*60*1000,max:3000})
+app.use(limiter)
+
+app.set('etag', false);
+app.use("/api/v1",router)
+
+
+// Add React Front End Routing
+app.get('*',function (req,res) {
+    res.sendFile(path.resolve(__dirname,'client','dist','index.html'))
+})
+
+module.exports=app;
---
+++ index.js
@@ -0,0 +1,6 @@
+const app=require("./app");
+const PORT=5000;
+app.listen(PORT,function () {
+    console.log("App is running on Port @5000")
+})
+
